1a05cf32a3e43a388114f6704728e060c7c476e6
CRayCodes <mr.coreyrbrown@gmail.com>
2023-04-07 20:43:55 -0400

    trying to fix deployment

--- client/script.js
+++ client/script.js
@@ -77,7 +77,7 @@ const handleSubmit = async (e) => {
 
   //fetch data from server -> bots response
 
-  const response = await fetch('https://cray-codex.onrender.com', {
+  const response = await fetch('http://localhost:5000', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
